ec7787d7e90354e43d0baba5a42e7846b2f8f91c
Shrish <shrish@iiitmanipur.ac.in>
2022-05-04 15:32:58 +0530

    composibleNFT

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const cNFTaddress = '0x4152Fc590604ABA08582198E309140eC9D162b54';
 const Web3 = require("web3");
 const ERC20Address =  document.getElementById("ERC20Address");
 const MTK = '0x08268C6A177Cd529DEAB226829C739C93f463994'
+// const MTK = ERC20Address.value
 const ERC20ABI = require('./ERC20.json')
 let deployedNetworkERC20;
   init = async () => {
@@ -25,7 +26,7 @@ let deployedNetworkERC20;
       );
     
     accounts = await web3.eth.getAccounts();
-    console.log('Account',accounts[0])
+    console.log("Account",accounts[0]);
   };
 
   mintNFT = async () => {
@@ -43,15 +44,23 @@ let deployedNetworkERC20;
 const btnMintNFT = document.getElementById("btnCreateItem");
 btnMintNFT.onclick = mintNFT;
 
+Approve = async() => {
+  contractERC20 = new web3.eth.Contract(
+    ERC20ABI,
+    erc20token.value
+  );
+await contractERC20.methods
+  .approve(cNFTaddress, tokenamount1.value)
+  .send({ from: accounts[0] });
+console.log("approved");
+}
+const erc20token = document.getElementById("erc20token");
+const tokenamount1 = document.getElementById("tokenamount1");
+const btnApprove = document.getElementById("btnApprove");
+btnApprove.onclick = Approve;
+
   energizeWithERC20 = async () => {
-    contractERC20 = new web3.eth.Contract(
-        ERC20ABI,
-        ERC20Address.value
-      );
-    await contractERC20.methods
-      .approve(cNFTaddress, getTokenAmount.value)
-      .send({ from: accounts[0] });
-    console.log("approved");
+    
     const receipt = await cNFTmethods.methods
       .energizeWithERC20(
         accounts[0],
@@ -61,7 +70,7 @@ btnMintNFT.onclick = mintNFT;
       )
       .send({ from: accounts[0] });
     console.log(receipt);
-    console.log("Recieved!!");
+    console.log("Energized!!");
   };
   const getTokenAmount = document.getElementById("getTokenAmount");
   const getTokenId = document.getElementById("getTokenId");
@@ -77,4 +86,27 @@ btnMintNFT.onclick = mintNFT;
 //   };
 //   const btnMintERC20 = document.getElementById("btnCreateERC20");
 // btnMintERC20.onclick = mintERC20;
+
+
+ReleaseERC20 = async () => {
+  const receipt = await cNFTmethods.methods
+    .transferERC20(
+      TokenIdofNFT.value,
+      receverAddressofERC20.value,
+      Erc20address1.value,
+      sendTokenAmount.value
+    )
+    .send({ from: accounts[0] });
+  console.log(receipt);
+  console.log("Released!!");
+};
+Erc20address1 = document.getElementById("Erc20address1");
+const receverAddressofERC20 = document.getElementById("receverAddressofERC20");
+const sendTokenAmount = document.getElementById("sendTokenAmount");
+const TokenIdofNFT = document.getElementById("TokenIdofNFT");
+
+const btnSendToken = document.getElementById("btnSendToken");
+btnSendToken.onclick = ReleaseERC20;
+
+
   init();
